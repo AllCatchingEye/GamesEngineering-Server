40351ef7539f90921ed1e5218ad60e32024e8d5c
Georg Lang <langgeorg61@gmail.com>
2023-10-21 21:17:23 +0200

    implemented gametypes enum

---
+++ src/state/gametypes.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Gametype(Enum):
+    # The value of the enum is the stake
+    Solo = 5
+    Wenz = 5
+    Geier = 5
+    Farbwenz = 3
+    Farbgeier = 3
+    Sauspiel = 1
