c1da986279b946249a7697d55380d6afc8d92103
Georg Lang <langgeorg61@gmail.com>
2023-10-21 21:17:47 +0200

    implemented basic card

--- src/state/cards.py
+++ src/state/cards.py
@@ -1,7 +1,13 @@
+from state.ranks import Rank
+from state.suits import Suit
+
 class Card:
-    def __init__(self, rank: str, suit: str):
+    def __init__(self, rank: Rank, suit: Suit):
         self.suit = suit
         self.rank = rank
 
-    def __str__(self):
-        return self.rank + " of " + self.suit
+    def get_value(self) -> int:
+        return self.rank.value
+
+    # def __str__(self):
+    #     return self.rank + " of " + self.suit
