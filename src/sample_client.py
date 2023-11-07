31a42160ce1748f700ed05de315a0c144ab43d0a
Envy <langgeorg61@gmail.com>
2023-11-07 10:53:26 +0100

    Implemented sample webserver

--- src/sample_client.py
+++ src/sample_client.py
@@ -1,30 +1,87 @@
-# import asyncio
+import asyncio
 import json
-from websockets.sync.client import connect
+import sys
+import websockets
+from websockets.client import WebSocketClientProtocol
+from websockets.typing import Data
+
+# Just for testing global
+join_key_g = ""
+
+async def start_client(type: str) -> None:
+    uri = "ws://localhost:8765"
+
+    async with websockets.connect(uri) as websocket:
+        print("Starting client...")
+        while True:
+            await run_client(websocket, type)
+
+async def run_client(websocket: WebSocketClientProtocol, type: str) -> None:
+    if type == "start":
+        await start(websocket, type)
+    else:
+        await join(websocket)
 
 
-def hello() -> None:
-    with connect("ws://localhost:8765") as websocket:
+async def start(websocket: WebSocketClientProtocol, type: str) -> None:
+    print("Asking server to start new game...")
+    event: dict[str, str] = {
+        "type": type
+    }
 
-        # Text communication example
-        websocket.send("Hello World")
-        message = websocket.recv()
-        print(f"Client Received: {message}")
+    message: str = json.dumps(event)
+    await websocket.send(message)
 
-        # Binary communication example
-        binaryData: bytes = int.to_bytes(6)
-        websocket.send(binaryData)
-        binary = websocket.recv()
-        print(f"Client Received binary: {binary}")
+    response: dict[str, str | int] = json.loads(await websocket.recv())
+    join_key_g: str = response["join_key"]
+    id: int = response["id"]
+    print(f"Server created new game with key {join_key_g}")
 
-        # Json communication example
-        dict = {
-            "name": "Json test",
-        }
-        json_object = json.dumps(dict)
-        websocket.send(json_object)
-        json_object = websocket.recv()
-        print(f"Client Received json: {json_object}")
+    await play(websocket, id)
+    await websocket.wait_closed()
+
+async def join(websocket: WebSocketClientProtocol) -> None:
+    print(f"Asking server to join game with key {join_key_g}")
+    event: dict[str, str] = {
+        "type": "join",
+        "join_key": join_key_g
+    }
+
+    message = json.dumps(event)
+    await websocket.send(message)
+
+    response: Data = await websocket.recv()
+    id_assignment: dict[str, int] = json.loads(response)
+    id: int = id_assignment["id"]
+
+    await play(websocket, id)
+
+    await websocket.wait_closed()
 
 
-hello()
+async def play(websocket: WebSocketClientProtocol, id: int) -> None:
+    async for message in websocket:
+        gamestate = parse_message(message)
+        turn_id = gamestate["turn_id"]
+        print(f"Player {id} received gamestate: {gamestate}")
+
+        # Just for testing
+        if turn_id == id:
+            input("Enter a action")
+            await send_action(websocket)
+
+def parse_message(message: Data) -> dict[str, str | int]:
+    gamestate: dict[str, str | int] = json.loads(message)
+    return gamestate
+
+async def send_action(websocket: WebSocketClientProtocol) -> None:
+    action = {
+        "action": "dummy_action"
+    }
+    message = json.dumps(action)
+    await websocket.send(message)
+
+type: str = sys.argv[1]
+if len(sys.argv) > 2:
+    join_key_g = sys.argv[2]
+asyncio.run(start_client(type))
--- src/sample_server.py
+++ src/sample_server.py
@@ -1,15 +1,19 @@
 import asyncio
 
+import json
+from websockets.typing import Data
+
 # import logging
 from websockets.server import WebSocketServerProtocol, serve
+from websockets import broadcast
+from logic.game import Game
+from secrets import token_urlsafe
 
+CLIENT_COUNT = 4
+free_id = 0
+active_clients = 0
 
-async def handler(websocket: WebSocketServerProtocol) -> None:
-    async for message in websocket:
-        print(f"Server received: {message}")
-        print(f"Server sends back message: {message}")
-        await websocket.send(message)
-
+JOIN: dict[str, tuple[Game, set[WebSocketServerProtocol]] ]= dict()
 
 async def main() -> None:
     print("Websocket starts")
@@ -17,10 +21,97 @@ async def main() -> None:
         print("Websocket runs")
         await asyncio.Future()  # run forever
 
+async def handler(websocket: WebSocketServerProtocol) -> None:
+    message: Data = await websocket.recv()
+    event: dict[str, str] = json.loads(message)
 
-def get_gamestate():
-    pass
+    if event["type"] == "start":
+        await start(websocket)
+    else:
+        join_key: str = event["join_key"]
+        await join(websocket, join_key)
 
+async def start(websocket: WebSocketServerProtocol) -> None:
+    global free_id, active_clients
+    connected: set[WebSocketServerProtocol] = {websocket}
+
+    game: Game = Game()
+    
+    join_key: str = token_urlsafe(12)
+    JOIN[join_key] = (game, connected)
+
+    print(f"Starting new game with key: {join_key}")
+    try:
+        event = {
+            "join_key": join_key,
+            "id": 0
+        }
+        free_id += 1
+        active_clients += 1
+
+        await websocket.send(json.dumps(event))
+        await play(websocket, connected)
+    finally:
+        del JOIN[join_key]
+
+async def join(websocket: WebSocketServerProtocol, join_key: str) -> None:
+    global free_id, active_clients
+    print(f"Client joined lobby with key: {join_key}")
+    lobby: tuple[Game, set[WebSocketServerProtocol]] = JOIN[join_key]
+    connected: set[WebSocketServerProtocol] = lobby[1]
+    connected.add(websocket)
+
+    id_assignment: dict[str, int] = {
+        "id": free_id
+    }
+    free_id += 1
+    active_clients += 1
+    message: str = json.dumps(id_assignment)
+    await websocket.send(message)
+
+    await play(websocket, connected)
+    try:
+        await websocket.wait_closed()
+    finally:
+        connected.remove(websocket)
+
+async def play(websocket: WebSocketServerProtocol, connected: set[WebSocketServerProtocol]) -> None:
+    turn_id = 0
+    await start_round(connected)
+
+    async for message in websocket:
+        action = json.loads(message)
+
+        gamestate = execute_action(action, turn_id)
+        await update_clients(gamestate, connected)
+
+async def start_round(connected: set[WebSocketServerProtocol]) -> None:
+    gamestate: dict[str, str | int] = {
+        "turn_id": 0,
+        "gamestate": "dummy",
+    }
+    message = json.dumps(gamestate)
+    broadcast(connected, message )
+
+def parse_message(message: Data) -> dict[str, str | int]:
+    action: dict[str, str | int] = json.loads(message)
+    return action
+
+def execute_action(event: dict[str, str | int], turn_id: int) -> dict[str, str | int]:
+    action: object = event.get("action")
+    print(f"Executed: {action}")
+    return get_new_gamestate(turn_id)
+
+def get_new_gamestate(turn_id: int) -> dict[str, str | int]:
+    gamestate: dict[str, str | int] = {
+        "turn_id": turn_id,
+        "gamestate": "dummy"
+    }
+    return gamestate
+
+async def update_clients(gamestate: dict[str, str | int], connected: set[WebSocketServerProtocol]) -> None:
+    message: str = json.dumps(gamestate)
+    broadcast(connected, message)
 
 # logger = logging.getLogger('websockets')
 # logger.setLevel(logging.DEBUG)
