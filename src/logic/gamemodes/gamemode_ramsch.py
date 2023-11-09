a0adfa2dfc9d1653448296081a903ea46435d893
Erik Sirch <erik.sirch@hm.edu>
2023-11-09 22:02:37 +0100

    updated get_game_winner method for ramsch

--- src/logic/gamemodes/gamemode_ramsch.py
+++ src/logic/gamemodes/gamemode_ramsch.py
@@ -2,6 +2,7 @@ from logic.gamemodes.gamemode import GameMode
 from state.deck import DECK
 from state.ranks import Rank
 from state.suits import Suit
+from state.player import Player
 
 
 class GameModeRamsch(GameMode):
@@ -14,3 +15,20 @@ class GameModeRamsch(GameMode):
                 trumps_init.append(card)
 
         super().__init__(Suit.HERZ, trumps_init)
+
+    def get_game_winner(self, play_party: list[list[Player]]) -> tuple[list[Player], list[int]]:
+        """Determine the winner of the entire game."""
+        party_points = []
+        for i, party in enumerate(play_party):
+            for player in play_party[0]:
+                party_points[i] += player.points
+
+        game_loser_index = party_points.index(max(party_points))
+        if party_points[game_loser_index] == 120:
+            return play_party[game_loser_index], party_points
+        else:
+            winners = []
+            for i, party in enumerate(play_party):
+                if i != game_loser_index:
+                    winners += party
+            return winners, party_points
