100fa71448d33bd1b3e10741e83afb1136604d17
BissigNicolas <nicolas.bissig@hm.edu>
2023-12-02 15:46:34 +0100

    feat(#71): Basic Sauspiel Dealer

---
+++ src/ai/dealer/dealer.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+import random
+
+from state.card import Card
+from state.suits import Suit
+
+
+class Dealer(ABC):
+    rng: random.Random
+
+    def __init__(self, seed: int | None = None) -> None:
+        self.rng = random.Random(seed)
+        super().__init__()
+
+    @abstractmethod
+    def deal(self, suit: Suit | None) -> tuple[list[Card], list[list[Card]]]:
+        pass
---
+++ src/ai/dealer/dealer_sauspiel.py
@@ -0,0 +1,35 @@
+from ai.dealer.dealer import Dealer
+from ai.dealer.deck_manipulation import take, take_any_except, take_rank, take_trumps
+from logic.gamemodes.gamemode_sauspiel import GameModeSauspiel
+from state.card import Card
+from state.deck import DECK
+from state.ranks import Rank
+from state.suits import Suit
+
+
+class DealerSauspiel(Dealer):
+    def deal(self, suit: Suit | None) -> tuple[list[Card], list[list[Card]]]:
+        if suit is None or suit is Suit.HERZ:
+            raise ValueError("Suit must be one of EICHEL, GRAS, or SCHELLEN")
+
+        trumps = GameModeSauspiel(suit).trumps
+
+        deck = DECK.get_full_deck()
+        self.rng.shuffle(deck)
+
+        good_cards: list[Card] = []
+
+        good_cards.append(take_rank(deck, Rank.UNTER))
+        good_cards.append(take_rank(deck, Rank.OBER))
+
+        good_cards.extend(take_trumps(deck, trumps, 2))
+
+        sau = Card(suit, Rank.ASS)
+        good_cards.extend(take_any_except(deck, [sau], 8 - len(good_cards)))
+
+        other_cards: list[list[Card]] = []
+
+        for _ in range(3):
+            other_cards.append(take(deck, 8))
+
+        return good_cards, other_cards
