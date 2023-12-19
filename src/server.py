8c32d19742a8e84df5c779ec41822ef412067fd7
Antonino Grasso <antonino.grasso@device-insight.com>
2023-12-19 16:32:21 +0000

    feat: add multiplayer

--- src/server.py
+++ src/server.py
@@ -2,8 +2,13 @@ import asyncio
 import json
 import logging
 import os
+import random
+import string
+from numpy import append
 
 from websockets import Data, WebSocketServerProtocol, serve
+from controller import websocket_controller
+from controller.delayed_controller import DelayedController
 
 from controller.delayed_controller import DelayedController
 from controller.passive_controller import PassiveController
@@ -14,6 +19,7 @@ from logic.game import Game
 TIMEOUT_SECS = 5 * 60  # 5 Minutes timeout
 CLIENTS: set[WebSocketServerProtocol] = set()
 
+LOBBIES: dict[str, set[WebSocketController]] = {}
 
 async def main() -> None:
     host = os.environ.get("HOST", "localhost")
@@ -38,20 +44,20 @@ async def handler(ws: WebSocketServerProtocol) -> None:
             if lobby_type == "single":
                 ## create single player lobby with 3 bots
                 await single_player_lobby(ws)
-
-                ## add player to game
-
-                ## send back single player lobby information
             elif lobby_type == "multi":
-                raise NotImplementedError("Multiplayer not implemented")
+                await start_multi_player_lobby(ws)
             else:
                 await ws.send("Unknown lobby type")
         case "lobby_join":
-            raise NotImplementedError("Multiplayer not implemented")
+            lobby_id = message["lobby_id"]
+            LOBBIES[lobby_id].add(WebSocketController(ws))
+            await ws.send("Joined lobby")
+
+            if len(LOBBIES[lobby_id]) == 4:
+                await multi_player_lobby(lobby_id)
         case _:
             msg = {key: "input_error", "message": "Unknown message"}
             await ws.send(msg)
-
 
 async def single_player_lobby(ws: WebSocketServerProtocol) -> None:
     game: Game = create_single_player_game(ws)
@@ -59,6 +65,25 @@ async def single_player_lobby(ws: WebSocketServerProtocol) -> None:
     await game.run(games_to_play=1)
     await ws.wait_closed()
 
+async def start_multi_player_lobby(ws: WebSocketServerProtocol) -> None:
+    lobby_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    LOBBIES[lobby_id] = set()
+    await ws.send(lobby_id)  # TODO
+
+    LOBBIES[lobby_id].add(WebSocketController(ws))
+
+async def multi_player_lobby(lobby_id: str) -> None:
+    game: Game = create_multi_player_game(lobby_id)
+
+    await game.run(games_to_play=1)
+    # TODO do we need ws.wait_closed() here?
+
+def create_multi_player_game(lobby_id: str) -> Game:
+    game: Game = Game()
+    s = LOBBIES[lobby_id]
+    game.controllers = list(map(lambda x: x, s))
+
+    return game
 
 def create_single_player_game(ws: WebSocketServerProtocol) -> Game:
     game: Game = Game()
@@ -69,7 +94,6 @@ def create_single_player_game(ws: WebSocketServerProtocol) -> Game:
         DelayedController(PassiveController()),
     ]
     return game
-
 
 if __name__ == "__main__":
     logging.basicConfig(
